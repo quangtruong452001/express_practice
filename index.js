6061ba71af23a0f1490be4ade7346d4db829d2e2
TruongDangQuang <quangtruong452001@gmail.com>
2024-05-20 15:57:37 +0700

    Update: add dotenv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import app from './src/app.js';
-const port = process.env.DEV_APP_PORT || 3000;
+import 'dotenv/config';
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
   console.log(`Server start with port: ${port}`);
